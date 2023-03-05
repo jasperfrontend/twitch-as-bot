60ad3cacc8b63cdc29d05df151e36a49fe1fa829
Jasper van der Meer <jaspervdmeer@gmail.com>
2023-03-05 22:05:03 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ debug: true });
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	options: { debug: true },
+	identity: {
+		username: process.env.CHANNEL,
+		password: process.env.AUTH
+	},
+	channels: [ process.env.CHANNEL ]
+});
+
+
+client.connect();
+setInterval(() => {
+    var textArray = [
+        'This stream is powered by https://antiscuff.com - The most powerful and easy to use OBS Cloud Hosting.',
+        'This livestream is powered by https://antiscuff.com - Affordable, safe and powerful OBS Cloud Hosting.',
+        'Here is another sentence.',
+        'Here is even another sentence.'
+    ];
+    var randomString = Math.floor(Math.random()*textArray.length);
+    client.say('jasperdiscovers', textArray[randomString]);
+}, 10000);
